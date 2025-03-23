98981e7087eee7ad9fbdf6a01a8c37cfbbd4fead
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-03-23 19:36:45 -0300

    create main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,20 @@
-import {Navlink} from "@/components/Navbar/Navlink";
-import {Navbar} from "@/components/Navbar";
 import {Hero} from "@/components/Sections/Hero";
 import {About} from "@/components/Sections/About";
 import {Features} from "@/components/Sections/Features";
 import {Services} from "@/components/Sections/Services";
 import {Testimonials} from "@/components/Sections/Testimonials";
 import {Contact} from "@/components/Sections/Contact";
-import {Footer} from "@/components/Sections/Footer";
+import {MainLayout} from "@/layouts/MainLayout";
 
 export default function Home() {
   return (
-    <>
-        <Navbar>
-            <Navlink href="/" isActive>Início</Navlink>
-            <Navlink href="/#sobre">Sobre nós</Navlink>
-            <Navlink href="/#servicos">Serviços</Navlink>
-            <Navlink href="/#depoimentos">Depoimentos</Navlink>
-            <Navlink href="/#preco">Preço</Navlink>
-            <Navlink href="/#contato">Contato</Navlink>
-        </Navbar>
+    <MainLayout>
         <Hero/>
         <About/>
         <Features/>
         <Services/>
         <Testimonials/>
         <Contact/>
-        <Footer/>
-    </>
+    </MainLayout>
   );
 }
